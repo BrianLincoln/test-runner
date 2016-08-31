0c5bbbfdd872804ed52b6f4b43e6fe1dad388d15
BrianCraddock <BrianLCraddock@gmail.com>
2016-08-30 21:59:41 -0400

    error checking. prevents tests hanging, will eventually be used for reporting back to user

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var webdriverio = require('webdriverio');
+var mocha = require('wdio-mocha-framework');
+var assert = require('assert');
 
 module.exports = function(flow) {
-
     var stepDelay = 0;
     var options = {
         desiredCapabilities: {
@@ -27,30 +28,70 @@ module.exports = function(flow) {
         switch (step.stepType) {
             case "pageLoad":
                 return this
-                  .url(step.url).pause(stepDelay);
+                  .url(step.url)
+                  .then(function(){
+                      //not called
+                  }, function(e) {
+                      throw new Error("failed to load url");
+                  })
+                  .pause(stepDelay);
                 break;
             case "click":
                 return this
                     .waitForVisible(step.selector)
-                    .click(step.selector).pause(stepDelay);
+                    .then(function(){
+                        //not called
+                    }, function(e) {
+                        throw new Error("failed to find element");
+                    })
+                    .click(step.selector)
+                    .then(function(){
+                        //not called
+                    }, function(e) {
+                        throw new Error("failed to click element");
+                    })
+                    .pause(stepDelay);
                 break;
             case "input":
                 return this
                     .waitForVisible(step.selector)
-                    .setValue(step.selector, step.inputValue).pause(stepDelay);
+                    .then(function(){
+                        //not called
+                    }, function(e) {
+                        throw new Error("failed to find element");
+                    })
+                    .setValue(step.selector, step.inputValue)
+                    .then(function(){
+                        //not called
+                    }, function(e) {
+                        throw new Error("failed to set value");
+                    })
+                    .pause(stepDelay);
                 break;
 
             case "confirmElementExists":
                 return this
-                    .waitForVisible(step.selector).pause(stepDelay)
+                    .waitForVisible(step.selector)
+                    .then(function(){
+                        //not called
+                    }, function(e) {
+                        throw new Error("failed to find element");
+                    })
+                    .pause(stepDelay);
                 break;
         }
     }
 
     function executeDynamicCommandsSequentially(commands) {
         var result = Promise.resolve();
-        commands.forEach(function (command) {
-            result = result.then(command);
+
+        commands.forEach(function (command, index) {
+            result = result.then(command)
+            .then(function(err){
+                //not called
+            }, function(err) {
+                throw new Error(err.message);
+            })
         });
         return result;
     }
@@ -59,6 +100,12 @@ module.exports = function(flow) {
         .generateDynamicCommands(flow.steps)
         .then(function(commands){
             return executeDynamicCommandsSequentially(commands);
+        })
+        .then(function(err){
+            //not called
+        }, function(err) {
+            console.log(err.message);
+            //return this or something
         })
         .end();
 
