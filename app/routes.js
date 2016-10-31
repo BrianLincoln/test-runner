2216514d654b6c0aa68885e669039364ba464eea
BrianCraddock <BrianLCraddock@gmail.com>
2016-10-31 18:04:42 -0400

    only run test if initial save succeeded

--- app/routes.js
+++ app/routes.js
@@ -6,26 +6,24 @@ module.exports = function(app) {
         //remove previous test(s) with matching flowId
         Test.remove({flowId: req.body.flow.id}, function(err) {
             if (!err) {
-                console.log("removed other tests")
+                var test = new Test({
+                    flowId: req.body.flow.id,
+                    start: Date.now(),
+                    status: "running"
+                });
+
+                test.save(function (err, test) {
+                    if (!err) {
+                        runTest(req.body.flow, test.id);
+                    }
+                    else {
+                        console.log(err);
+                        res.send(err);
+                    }
+                });
             }
             else {
                 console.log(err);
-            }
-        });
-
-        var test = new Test({
-            flowId: req.body.flow.id,
-            start: Date.now(),
-            status: "running"
-        });
-        
-        test.save(function (error, test) {
-            if (error) {
-                res.send(error);
-            }
-            else {
-                runTest(req.body.flow, test.id);
-                res.send('hello from the other side');
             }
         });
     });
